07dd8aaf8c8e6ccdbff4af913d29592acca33648
Hector Ponce <hectorponmor@gmail.com>
2022-06-17 15:11:22 -0500

    Agregué estilo nuevamente

--- Saludo/script.js
+++ Saludo/script.js
@@ -1,1 +1,14 @@
-console.log ("Se nota que todavía me extrañas")
+console.log ("Se nota que todavía me extrañas");
+
+setInterval(cambiarSaludo, 4000);
+
+function cambiarSaludo() {
+  
+  let frase = document.getElementsByTagName("h2")[1].innerHTML;
+  if (document.getElementsByTagName("h2")[1].innerHTML == "Ganando como siempre") {
+    document.getElementsByTagName("h2")[1].innerHTML = "Impongo moda para las niñas!";
+  } else {
+    document.getElementsByTagName("h2")[1].innerHTML = "Ganando como siempre";
+  }
+  console.log("frase :" + frase);
+}
