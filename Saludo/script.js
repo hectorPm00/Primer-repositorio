2734356f6f9dda8c0a497bb47e2de0de80fc0e63
Hector Ponce <hectorponmor@gmail.com>
2022-06-17 15:20:39 -0500

    Último cambio

--- Saludo/script.js
+++ Saludo/script.js
@@ -6,7 +6,7 @@ function cambiarSaludo() {
   
   let frase = document.getElementsByTagName("h2")[1].innerHTML;
   if (document.getElementsByTagName("h2")[1].innerHTML == "Ganando como siempre") {
-    document.getElementsByTagName("h2")[1].innerHTML = "Impongo moda para las niñas!";
+    document.getElementsByTagName("h2")[1].innerHTML = "¡Impongo moda para las niñas!";
   } else {
     document.getElementsByTagName("h2")[1].innerHTML = "Ganando como siempre";
   }
